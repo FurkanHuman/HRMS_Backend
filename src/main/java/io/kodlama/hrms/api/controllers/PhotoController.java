8141a813f6a03b104f839824e6660b687893f8e9
Furkan Bozkurt <furkan-b136@hotmail.com>
2021-06-23 02:57:14 +0300

    Swagger file uploader added

--- src/main/java/io/kodlama/hrms/api/controllers/PhotoController.java
+++ src/main/java/io/kodlama/hrms/api/controllers/PhotoController.java
@@ -1,27 +1,31 @@
 package io.kodlama.hrms.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.kodlama.hrms.business.abstracts.PhotoService;
+import io.swagger.annotations.ApiParam;
 
 @RestController
 @RequestMapping(value = "/api/Photo")
 public class PhotoController {
     private final PhotoService photoService;
 
+    @Autowired
     public PhotoController(PhotoService photoService) {
         this.photoService = photoService;
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> add(@RequestBody MultipartFile file, @RequestParam int candidateUserId) {
+    public ResponseEntity<?> add(@ApiParam(name = "file", value = "The file") @RequestPart MultipartFile file,
+            @RequestParam int candidateUserId) {
         return ResponseEntity.ok(this.photoService.add(file, candidateUserId));
     }
 
