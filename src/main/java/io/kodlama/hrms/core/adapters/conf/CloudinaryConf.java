9253febe0c9369b20e364be07b64edaf3695abb7
Furkan Bozkurt <furkan-b136@hotmail.com>
2021-06-24 09:44:51 +0300

    Bug fix and multiple usage added.

--- src/main/java/io/kodlama/hrms/core/adapters/conf/CloudinaryConf.java
+++ src/main/java/io/kodlama/hrms/core/adapters/conf/CloudinaryConf.java
@@ -22,39 +22,37 @@ public class CloudinaryConf {
         this.propertFile = propertFile;
     }
 
-    @Bean
+    // @Bean // propertFile.ReadPropert ile url yöntemi
+    // public Cloudinary cloudinaryConfig() throws IOException {
+    // return new Cloudinary(this.propertFile.ReadPropert("cloudinary_url"));
+    // }
+
+    @Bean // set yöntemi.
     public Cloudinary cloudinaryConfig() throws IOException {
 
         Cloudinary cloudinary = new Cloudinary();
+        cloudinary.config.cloudName = this.propertFile.ReadPropert("cloud_name");
         cloudinary.config.apiKey = this.propertFile.ReadPropert("api_key");
         cloudinary.config.apiSecret = this.propertFile.ReadPropert("api_secret");
-        cloudinary.config.cloudName = this.propertFile.ReadPropert("cloud_name");
-
+        cloudinary.config.secure = true; // HTTPS protokülünü kullanmak için gereken.
         return cloudinary;
-
     }
-    // @Bean
+
+    // @Bean // Düz url yöntemi. (Java EE) KULLANILABİLİR değil. araştırlıması
+    //// lazım.
+    // public Cloudinary cloudinaryConfig() {
+    // return new Cloudinary();
+    // }
+
+    // @Bean // map yöntemi
     // public Cloudinary cloudinaryConfig() throws IOException {
 
+    // // ObjectUtils.asMap() Cloudinaryden import edilecek
     // return new Cloudinary(ObjectUtils.asMap("cloud_name",
     // this.propertFile.ReadPropert("cloud_name"), "api_key",
     // this.propertFile.ReadPropert("api_key"), "api_secret",
     // this.propertFile.ReadPropert("api_secret")));
     // }
-
-    // @Bean
-    // public Cloudinary cloudinaryConfig() {
-    // return new Cloudinary(ObjectUtils.asMap("cloud_name", "dh6vglkc1", "api_key",
-    // "148529172354494", "api_secret",
-    // "pe1Y2TmRKTGV0rDN3Q-VpT1eomQ"));
-    // }
-
-    // @Bean
-    // public Cloudinary cloudinaryConfig() {
-    // return new Cloudinary(ObjectUtils.asMap("cloud_name",
-    // environment.getProperty("cloud_name").toString(),
-    // "api_key", environment.getProperty("api_key").toString(), "api_secret",
-    // environment.getProperty("api_secret").toString()));
-    // }
+    // // secure parametresi https protokolünü açar.
 
 }
