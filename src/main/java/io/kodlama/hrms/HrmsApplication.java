945ca7574076d9b397f39475f915bd4bc3a56c75
Furkan Bozkurt <furkan-b136@hotmail.com>
2021-06-24 07:25:09 +0300

    None changes

--- src/main/java/io/kodlama/hrms/HrmsApplication.java
+++ src/main/java/io/kodlama/hrms/HrmsApplication.java
@@ -3,7 +3,6 @@ package io.kodlama.hrms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -11,7 +10,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 @SpringBootApplication
 public class HrmsApplication {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
 
