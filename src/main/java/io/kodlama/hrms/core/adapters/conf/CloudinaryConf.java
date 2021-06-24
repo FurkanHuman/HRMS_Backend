f93b1fa3634c820b276af30695f9b768738d4f8a
Furkan Bozkurt <furkan-b136@hotmail.com>
2021-06-24 21:36:58 +0300

    fix and envorment usage added

--- src/main/java/io/kodlama/hrms/core/adapters/conf/CloudinaryConf.java
+++ src/main/java/io/kodlama/hrms/core/adapters/conf/CloudinaryConf.java
@@ -1,25 +1,38 @@
 package io.kodlama.hrms.core.adapters.conf;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import com.cloudinary.Cloudinary;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.kodlama.hrms.core.utilities.readProperty.ReadPropertFile;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CloudinaryConf {
 
-    private final ReadPropertFile propertFile;
+    private final Environment environment;
 
     @Autowired
-    public CloudinaryConf(ReadPropertFile propertFile) throws FileNotFoundException, IOException {
-        ReadPropertFile.propertiesPath = "src\\main\\resources\\application.properties";
-        this.propertFile = propertFile;
+    public CloudinaryConf(Environment environment) {
+        this.environment = environment;
+    }
+
+    // private final ReadPropertFile propertFile;
+
+    // public CloudinaryConf(ReadPropertFile propertFile, Environment environment)
+    // throws FileNotFoundException, IOException {
+    // ReadPropertFile.propertiesPath =
+    // "src\\main\\resources\\application.properties"; // ReadPropertFile
+    // Environment kullanılamadığı zaman kullanılabilir.
+    // this.propertFile = propertFile;
+    // this.environment = environment;
+    // }
+
+    @Bean // Düz url yöntemi. Environment class kullanıması ve enjekte edilmesi gerekiyor.
+    public Cloudinary cloudinaryConfig() {
+        System.out
+                .println("Cloudinary V " + Cloudinary.VERSION + " Java Version: " + Cloudinary.USER_AGENT_JAVA_VERSION);
+        return new Cloudinary(environment.getProperty("CLOUDINARY_URL"));
     }
 
     // @Bean // propertFile.ReadPropert ile url yöntemi
@@ -27,21 +40,14 @@ public class CloudinaryConf {
     // return new Cloudinary(this.propertFile.ReadPropert("cloudinary_url"));
     // }
 
-    @Bean // set yöntemi.
-    public Cloudinary cloudinaryConfig() throws IOException {
-
-        Cloudinary cloudinary = new Cloudinary();
-        cloudinary.config.cloudName = this.propertFile.ReadPropert("cloud_name");
-        cloudinary.config.apiKey = this.propertFile.ReadPropert("api_key");
-        cloudinary.config.apiSecret = this.propertFile.ReadPropert("api_secret");
-        cloudinary.config.secure = true; // HTTPS protokülünü kullanmak için gereken.
-        return cloudinary;
-    }
-
-    // @Bean // Düz url yöntemi. (Java EE) KULLANILABİLİR değil. araştırlıması
-    //// lazım.
-    // public Cloudinary cloudinaryConfig() {
-    // return new Cloudinary();
+    // @Bean // set yöntemi.
+    // public Cloudinary cloudinaryConfig() throws IOException {
+    // Cloudinary cloudinary = new Cloudinary();
+    // cloudinary.config.cloudName = this.propertFile.ReadPropert("cloud_name");
+    // cloudinary.config.apiKey = this.propertFile.ReadPropert("api_key");
+    // cloudinary.config.apiSecret = this.propertFile.ReadPropert("api_secret");
+    // cloudinary.config.secure = true; // HTTPS protokülünü kullanmak için gereken.
+    // return cloudinary;
     // }
 
     // @Bean // map yöntemi
