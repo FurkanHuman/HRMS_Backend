ab98e5df201b3f0777da1298d14c50277d4a4d0d
Furkan Bozkurt <furkan-b136@hotmail.com>
2021-10-06 03:00:19 +0300

    photo deletion and new rules added. error exists

--- src/main/java/io/kodlama/hrms/business/concretes/PhotoManager.java
+++ src/main/java/io/kodlama/hrms/business/concretes/PhotoManager.java
@@ -1,6 +1,9 @@
 package io.kodlama.hrms.business.concretes;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,6 +15,7 @@ import io.kodlama.hrms.core.utilities.results.DataResult;
 import io.kodlama.hrms.core.utilities.results.ErrorResult;
 import io.kodlama.hrms.core.utilities.results.Result;
 import io.kodlama.hrms.core.utilities.results.SuccessDataResult;
+import io.kodlama.hrms.core.utilities.results.SuccessResult;
 import io.kodlama.hrms.dataAccess.abstracts.CandidateUserDao;
 import io.kodlama.hrms.dataAccess.abstracts.PhotoDao;
 import io.kodlama.hrms.entities.concretes.Photo;
@@ -52,6 +56,21 @@ public class PhotoManager implements PhotoService {
         return allDataResult.getSuccessResults();
     }
 
+    public List<Result> delete(int userId) {
+        AllDataResult allDataResult = checkPhotoDatabase(userId);
+        if (!allDataResult.isSuccess())
+            return allDataResult.getErrorResults();
+
+        List<Photo> userPhotos = this.photoDao.findByUserId(userId);
+
+        for (Photo photo : userPhotos) {
+
+            this.imageService.remove(urlToId(photo.getLink())).equals("ok");
+            this.photoDao.delete(photo);
+        }
+        return allDataResult.getSuccessResults();
+    }
+
     private AllDataResult checkPhoto(MultipartFile file, int candidateUserId) {
         AllDataResult allDataResult = new AllDataResult();
 
@@ -59,7 +78,29 @@ public class PhotoManager implements PhotoService {
             allDataResult.addResult(new ErrorResult("dosya boş"));
         if (!this.candidateUserDao.existsById(candidateUserId))
             allDataResult.addResult(new ErrorResult("candidade id boş"));
-
+        if (this.photoDao.existsByUserId(candidateUserId))
+            allDataResult.addResult(new ErrorResult("sadece tek fotoğraf"));
         return allDataResult;
     }
+
+    private AllDataResult checkPhotoDatabase(int candidateUserId) {
+        AllDataResult allDataResult = new AllDataResult();
+
+        if (this.photoDao.getByUserId(candidateUserId) == null)
+            allDataResult.addResult(new ErrorResult("veri yok"));
+
+        if (this.photoDao.getByUserId(candidateUserId).getLink().isEmpty())
+            allDataResult.addResult(new ErrorResult("link bulunamadı"));
+        return allDataResult;
+    }
+
+    private String urlToId(String link) {
+
+        Pattern p = Pattern.compile("^.*/([a-zA-Z-0-9]+)...*$");
+        Matcher m = p.matcher(link);
+        if (m.find())
+            return m.group(1);
+        return null;
+    }
+
 }
