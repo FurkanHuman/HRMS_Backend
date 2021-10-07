e171daeb92565958aa19157a51c3189cb82f0c1b
Furkan Bozkurt <furkan-b136@hotmail.com>
2021-10-08 00:34:34 +0300

    databases just single photo. removed structure of List<>

--- src/main/java/io/kodlama/hrms/dataAccess/abstracts/PhotoDao.java
+++ src/main/java/io/kodlama/hrms/dataAccess/abstracts/PhotoDao.java
@@ -1,14 +1,12 @@
 package io.kodlama.hrms.dataAccess.abstracts;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.kodlama.hrms.entities.concretes.Photo;
 
 public interface PhotoDao extends JpaRepository<Photo, Integer> {
 
-    List<Photo> findByUserId(int candidateId);
+    Photo findByUserId(int candidateId);
 
     String findBylink(int photoId);
 
